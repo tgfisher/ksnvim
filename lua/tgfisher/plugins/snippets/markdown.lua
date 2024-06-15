fc1923f3d1120ffb06870f46174feace9b17c7b2
tgf <tuckergfisher@gmail.com>
2024-06-15 10:51:48 -0700

    checklist

--- lua/tgfisher/plugins/snippets/markdown.lua
+++ lua/tgfisher/plugins/snippets/markdown.lua
@@ -6,4 +6,8 @@ return {
     "### Monday $1\n\n### Tuesday $2\n\n### Wednesday $3\n\n"
       .. "### Thursday $4\n\n### Friday $5\n\n### Saturday $6\n\n### Sunday $0"
   ),
+  ls.parser.parse_snippet(
+    "- [ ]",
+    "- [$1] $2"
+  ),
 }
